460ba14eb75d8081cb431b37372cba616f83ab3a
Charbel t <charbel2000.52@gmail.com>
2020-12-08 06:27:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ let createEmployeeRecords = function(employeeRowData){
 };
 
 let createTimeInEvent = function(employee,dateStamp){
-  let [date,hour]=dateStamp.split(' ')
+  let [date,hour]=dateStamp.split(' ');
   employee.timeInEvents.push({
     type: "TimeIn",
     hour: parseInt(hour,10),
-    date
-  })
-}
+    date,
+  });
+};
 
 
 
