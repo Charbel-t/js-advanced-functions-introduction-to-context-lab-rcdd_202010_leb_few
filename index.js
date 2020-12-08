bf4a10de1468eb2546acbc7f3d1db59d268a7143
Charbel t <charbel2000.52@gmail.com>
2020-12-08 06:34:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ let createTimeOutEvent = function(employee,dateStamp){
 };
 
 let hoursWorkedOnDate = function(employee,date){
-  let inEvent=employee.time
+  let inEvent=employee.timeInEvents.find
 };
 
 
